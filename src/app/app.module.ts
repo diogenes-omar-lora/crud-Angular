51a10841572645f3c1c68a9a75146cc4fa71ed4a
diogenes-omar-lora <omarlora08@hotmail.com>
2022-10-11 21:49:39 -0400

    fase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
